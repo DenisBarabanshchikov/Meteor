fee7df1376d15f6eb2550de0a02a20db6c52aff5
Denis Barabanshchikov <lord_dizel@mail.ru>
2016-01-07 12:23:03 +0400

    Week 2
    Update application
    added modal form for add images

--- image_share/image_share.js
+++ image_share/image_share.js
@@ -3,7 +3,9 @@ console.log(Images.find().count());
 
 if (Meteor.isClient) {
 
-    Template.images.helpers({images: Images.find()});
+    Template.images.helpers({images: 
+        Images.find({}, {sort:{cratedOn:-1, rating:-1}})
+    });
     
     Template.images.events({
         'click .js-image':function(event){
@@ -11,7 +13,6 @@ if (Meteor.isClient) {
         },
         'click .js-del-image':function(event){
             var image_id = this._id;
-            console.log(image_id);
             $('#'+image_id).hide('slow', function(){
                 Images.remove({"_id" : image_id});
             });
@@ -19,7 +20,30 @@ if (Meteor.isClient) {
         },
         'click .js-rate-image':function(event){
             var rating = $(event.currentTarget).data("userrating");
-            console.log(rating);
+            var image_id = $(this).attr("data-mongo-id");
+            Images.update({_id:image_id}, {$set: {rating:rating}});
+        },
+        'click .js-show-image-form':function(event){
+            $('#image_add_form').modal('show');
+        }
+    });
+    
+    Template.image_add_form.events({
+        'submit .js-add-image':function(event){
+            var image_src, image_alt;
+            image_src = event.target.img_src.value;
+            image_alt = event.target.img_alt.value;
+            console.log('src: '+image_src+" alt: "+image_alt);
+            
+            Images.insert(
+                {
+                img_src:image_src,
+                img_alt:image_alt,
+                cratedOn:new Date()
+                }
+            );
+            
+            return false;
         }
     });
 }
